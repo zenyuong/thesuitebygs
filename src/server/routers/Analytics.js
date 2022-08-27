5efd6184c38e73fa40ca3876aa27125be3a1afd0
Zen Yu  Ong <ongzenyu112@gmail.com>
2022-08-27 17:36:16 +0800

    visualise data endpoint done

--- src/server/routers/Analytics.js
+++ src/server/routers/Analytics.js
@@ -1,23 +1,57 @@
 const router = require("express").Router();
 const axios = require("axios");
 var csv = require("fast-csv");
+const multer = require("multer");
+const fs = require("fs");
 
 module.exports = () => {
-  router.post("/visualise-data", (req, res) => {
-    let companyInfo = [];
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./uploads/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+    },
+  });
 
-    csv
-      .parseFile("./routers/upcomingPublicOffering.csv", { headers: true })
-      .on("data", function (data) {
-        companyInfo.push(data);
-      })
-      .on("end", function () {
-        return res.send({
-          ok: true,
-          msg: "Data Extracted From CSV",
-          companyInfo,
+  const csvFilter = (req, file, cb) => {
+    if (file.mimetype.includes("csv")) {
+      cb(null, true);
+    } else {
+      cb("Please upload only csv file.", false);
+    }
+  };
+
+  const upload = multer({ storage: storage, fileFilter: csvFilter });
+
+  router.post("/visualise-data", upload.single("file"), (req, res) => {
+    console.log("hihi", req.file);
+
+    try {
+      if (req.file == undefined) {
+        return res
+          .status(400)
+          .send({ ok: false, msg: "Please upload a CSV file" });
+      }
+
+      let companyInfo = [];
+      let filePath = `./uploads/${req.file.filename}`;
+      csv
+        .parseFile(filePath, { headers: true })
+        .on("data", function (data) {
+          companyInfo.push(data);
+          console.log(data);
+        })
+        .on("end", function () {
+          return res.send({
+            ok: true,
+            msg: "Data Extracted From CSV",
+            companyInfo,
+          });
         });
-      });
+    } catch (e) {
+      return res.send({ ok: false, e });
+    }
   });
 
   router.get("/live-market-statistics", (req, res) => {
