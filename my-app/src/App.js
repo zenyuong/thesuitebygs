3e15a86626e621dec6e0eec71e4a121f07e2271f
FIRST_NAME LAST_NAME <89060200+sophiaaachow@users.noreply.github.com>
2022-08-27 13:40:06 +0800

    Add default sidebar, bootstrap css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div>
-      
+      <Sidebar></Sidebar>
     </div>
   );
 }
