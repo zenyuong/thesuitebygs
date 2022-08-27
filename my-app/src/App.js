55f96d9f89a605eab2022696462a33a64766ff59
JAYE YAP <jayeyap.2020@scis.smu.edu.sg>
2022-08-27 15:35:25 +0800

    functional ConsultModal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,28 @@
 /* CSS */
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import React from 'react'
 
 /* Components */
 import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
+import ConsultModal from './components/ConsultModal';
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <div className='App'>
       <Sidebar></Sidebar>
-      <Footer></Footer>
+      <ConsultModal></ConsultModal>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Consult an Expert
+      </Button>
+
+      <ConsultModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 }
